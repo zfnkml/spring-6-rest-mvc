f18a7eef7980eb9766a31e0baa6fab76d310ca3e
Nafiz Kamal <nfzkml11.cs@gmail.com>
2024-08-01 17:01:18 +0600

    add model.Beer

---
+++ src/main/java/guru/springframework/spring6restmvc/model/Beer.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6restmvc.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/*
+ ** 2024, August 01, Thursday, 4:57 PM
+ */
+
+public class Beer {
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
---
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6restmvc.model;
+
+/*
+ ** 2024, August 01, Thursday, 4:57 PM
+ */
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
